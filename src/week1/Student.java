1b3eee8ae1a91afdddf41269ecf6e2839e09ee63
minhphuong16px <minhphuong16px@gmail.com>
2018-09-21 12:48:21 +0700

    Student

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,16 +1,48 @@
 package week1;
 
 public class Student {
-
     // TODO: khai báo các thuộc tính cho Student
+    private String name, id, email, group;
 
     // TODO: khai báo các phương thức getter, setter cho Student
+    public String getName(){
+        return name;
+    }
+    public String getId (){
+        return id;
+    }
+    public String getEmail (){
+        return email;
+    }
+    public String getGroup (){
+        return  group;
+    }
+    public void setName (String n){
+        this.name = n;
+    }
+    public void setId (String sid) {
+        this.id  = sid;
+    }
+    public  void setEmail (String em){
+        this.email = em;
+    }
+    public  void  setGroup (String nhom) {
+        this.group = nhom;
+    }
+
+
+
+
 
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        this.name = "Student";
+        this.id = "000";
+        this.email = "uet@vnu.edu.vn";
+        this.group = "“INT22041";
     }
 
     /**
@@ -21,6 +53,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name = n;
+        this.id = sid;
+        this.email = em;
+        group = "INT2207";
     }
 
     /**
@@ -29,10 +65,16 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.email = s.email;
+        this.name = s.name;
+        this.group = s.group;
+        this.id = s.id;
     }
 
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+    public void getInfo() {
+        System.out.println(this.getId());
+        System.out.println(this.getName());
+        System.out.println(this.getGroup());
+        System.out.println(this.getEmail());
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,74 @@
 package week1;
 
-public class StudentManagement {
+import com.sun.deploy.panel.DeleteFilesDialog;
 
+import javax.xml.bind.SchemaOutputResolver;
+
+public class StudentManagement {
+    Student[] students= new Student[100];
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
 
     public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        // TODO:kiem tra hai sinh vien co cung lop hay khong
+        //return false; // xóa dòng này sau khi cài đặt
+        if(s1.getGroup().equals(s2.getGroup())) {
+            return true;
+        }
+
+        return false;
     }
 
     void studentsByGroup() {
         // TODO:
+
+        System.out.println();
     }
 
     void removeStudent(String id) {
-        // TODO:
+        Student s= new Student();
+        if(s.getId()== id){
+        }
     }
 
     public static void main(String[] args) {
-        // TODO:
+        // TODO: Thong tin ban than
+
+        Student s= new Student();
+        s.setId("17020972");
+        s.setName("Phuong");
+        s.setGroup("K62 UET");
+        s.setEmail("uet@vnu.vn");
+        System.out.println("Ten Sinh Vien: " +s.getName());
+        s.getInfo();
+        /* Cau 8
+        Student s = new Student();
+        s.getInfo();
+
+        Student s1 = new Student("phuong", "01", "phuong@gmail.com");
+        s1.setGroup("K62 UET");
+        s1.getInfo();
+
+        Student s2 = new Student(s1);
+        s2.getInfo();
+        */
+//      Student s = new Student();
+//      s.getInfo();
+        Student s1 = new Student("abc", "01", "abc@gmail.com");
+        s1.setGroup("INT22041");
+        s1.getInfo();
+        Student s2 = new Student("def", "02", "def@gmail.com");
+        s2.setGroup("INT22041");
+        s2.getInfo();
+        Student s3 = new Student("xyz", "03", "xyz@gmail.com");
+        s3.setGroup("INT22042");
+        s3.getInfo();
+        StudentManagement management = new StudentManagement();
+        System.out.println("Sinh vien s1 vs sinh vien s2 cung lop: " +management.sameGroup(s1, s2));
+        System.out.println("Sinh vien s2 vs sinh vien s3 cung lop: " +management.sameGroup(s2, s3));
+        System.out.println("Sinh vien s1 vs sinh vien s3 cung lop: " +management.sameGroup(s1, s3));
+
+
+
+
     }
 }
